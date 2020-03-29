c3068ddb5bfde2975f6b0705d8e41466f3edea6e
Bengt Marten Agren <maraingist@me.com>
2020-03-29 15:06:02 +0800

    Added timer interrupt with auto closing of channels.

--- WaterGate.ino
+++ WaterGate.ino
@@ -2,6 +2,7 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <Ticker.h>
 #include <ArduinoJson.h>
 #include "Content.h"
 
@@ -17,14 +18,66 @@ const int OUTPUT2 = 13;
 const int OUTPUT1 = 4;
 const int OUTPUT0 = 5;
 
+template<int CHAN>
+struct output_t
+{
+    static const int max_count = 500;   // 50Hz -> 10s
+
+    static bool read()
+    {
+        return digitalRead(CHAN);
+    }
+
+    static void write(bool x)
+    {
+        digitalWrite(CHAN, x);
+        count = 0;
+    }
+
+    static void update()
+    {
+        if (count == max_count)
+            digitalWrite(CHAN, 0);
+        else if (count < max_count)
+            ++count;
+    }
+
+    static volatile int count;
+};
+
+template<int CHAN>
+volatile int output_t<CHAN>::count = 0;
+
+typedef output_t<OUTPUT0> ch0;
+typedef output_t<OUTPUT1> ch1;
+typedef output_t<OUTPUT2> ch2;
+typedef output_t<OUTPUT3> ch3;
+
+void ICACHE_RAM_ATTR onTimerISR()
+{
+    static int i = 0;
+
+    if ((i & 0x3f) == 0)
+        digitalWrite(STATUS_LED, true);
+    else if ((i & 0x3f) == 8)
+        digitalWrite(STATUS_LED, false);
+
+    ch0::update();
+    ch1::update();
+    ch2::update();
+    ch3::update();
+
+    ++i;
+}
+
 void handleRoot()
 {
     Serial.println("/");
     String buf = String(main_form_preamble)
-               + main_form_button(0, digitalRead(OUTPUT0))
-               + main_form_button(1, digitalRead(OUTPUT1))
-               + main_form_button(2, digitalRead(OUTPUT2))
-               + main_form_button(3, digitalRead(OUTPUT3))
+               + main_form_button(0, ch0::read())
+               + main_form_button(1, ch1::read())
+               + main_form_button(2, ch2::read())
+               + main_form_button(3, ch3::read())
                + String(main_form_postamble);
 
     digitalWrite(STATUS_LED, 1);
@@ -61,6 +114,11 @@ void setup(void){
     WiFi.begin(ssid, password);
     Serial.println("");
 
+    // Timer setup
+    timer1_attachInterrupt(onTimerISR);
+    timer1_enable(TIM_DIV16, TIM_EDGE, TIM_LOOP);
+    timer1_write(100000);   // 50Hz
+
     // Wait for connection
     while (WiFi.status() != WL_CONNECTED)
     {
@@ -85,40 +143,40 @@ void setup(void){
     MDNS.addServiceTxt("http", "tcp", "webthing", "true");
 
     server.on("/", handleRoot);
-  
+
     server.on("/0", [](){
         Serial.println("/0");
         server.send(200, "text/html", redirect_home);
-        digitalWrite(OUTPUT0, !digitalRead(OUTPUT0));
+        ch0::write(!ch0::read());
     });
-  
+
     server.on("/1", [](){
         Serial.println("/1");
         server.send(200, "text/html", redirect_home);
-        digitalWrite(OUTPUT1, !digitalRead(OUTPUT1));
+        ch1::write(!ch1::read());
     });
-  
+
     server.on("/2", [](){
         Serial.println("/2");
         server.send(200, "text/html", redirect_home);
-        digitalWrite(OUTPUT2, !digitalRead(OUTPUT2));
+        ch2::write(!ch2::read());
     });
-  
+
     server.on("/3", [](){
         Serial.println("/3");
         server.send(200, "text/html", redirect_home);
-        digitalWrite(OUTPUT3, !digitalRead(OUTPUT3));
+        ch3::write(!ch3::read());
     });
-  
+
     server.on("/clr", [](){
         Serial.println("/clr");
         server.send(200, "text/html", redirect_home);
-        digitalWrite(OUTPUT0, 0);
-        digitalWrite(OUTPUT1, 0);
-        digitalWrite(OUTPUT2, 0);
-        digitalWrite(OUTPUT3, 0);
+        ch0::write(false);
+        ch1::write(false);
+        ch2::write(false);
+        ch3::write(false);
     });
-  
+
     server.onNotFound(handleNotFound);
     server.begin();
     Serial.println("HTTP server started");
@@ -135,4 +193,3 @@ void loop(void)
     else if (i == 1024)
         digitalWrite(ESP_LED, 1);
 }
-
